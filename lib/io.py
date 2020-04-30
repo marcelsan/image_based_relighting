b4f6deecd6612932d4436c68d4f8db09d1d4572c
Marcel Santos <mss8@cin.ufpe.br>
2020-04-30 18:36:25 -0300

    Several adjuts on the code.

--- lib/io.py
+++ lib/io.py
@@ -1,4 +1,5 @@
 import glob
+import os
 
 import cv2
 import numpy as np
@@ -6,24 +7,36 @@ import pyexr
 
 from lib.geometry import FromCartesianToSphericalCoordinates
 
-def ReadReflectanceField(folder, mask_path):
-	images = glob.glob(folder + '/*')
-	ret = [pyexr.read_all(file)['default'] for file in images]
+def UCSDSortDirectory(e):
+	return int(os.path.basename(e).split("_")[0])
+
+def ReadReflectanceField(folder, file_format='png', mask_path=None):
+	images = glob.glob(os.path.join(folder, '*'))
+	images.sort(key=UCSDSortDirectory)
+
+	ret = []
+	for im_file in images:
+		if file_format in ['png', 'jpg', 'jpeg', 'hdr']:
+			im = cv2.imread(im_file)[..., ::-1] / 255.0
+		elif file_format in ['exr']:
+			im = pyexr.read_all(im_file)['default']
+		else:
+			print("No supported file format {}".format(file_format))
+			break
+
+		# Undo gamma correction
+		no_gamma = np.power(im, 2.2)
+		ret.append(no_gamma)
 
 	# Read mask
-	mask = cv2.imread(mask_path, 0)/255.0
+	mask = None
+	if mask_path:
+		mask = cv2.imread(mask_path, 0)/255.0
 
 	return ret, mask
 
 def ReadLightDirections(file):
-	directions = []
-
-	f = open(file, "r")
-	for l in f:
-		pos = [-float(c) for c in l.rstrip().split(" ")[1:]]
-		directions.append(pos)
-
-	return np.array(directions)
+	return np.loadtxt(file)
 
 def ReadLightIntensities(file):
 	ret = []
@@ -35,9 +48,12 @@ def ReadLightIntensities(file):
 
 	return np.array(ret)
 
-def ReadLightsInfo(intensities_dir, directions_dir):
-	intensities = ReadLightIntensities(intensities_dir)
+def ReadLightsInfo(directions_dir, intensities_dir=None):
 	directions = ReadLightDirections(directions_dir)
+	if intensities_dir:
+		intensities = ReadLightIntensities(intensities_dir)
+	else:
+		intensities = np.ones_like(directions)
 
 	# convert light directions from cartesian to spherical coordinates 
 	directions = FromCartesianToSphericalCoordinates(directions)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 import cv2
 import numpy as np
 import pyexr
+import time
 
 from lib.environment_map import ConvertCubeMap2LL
 from lib.geometry import FromCartesianToSphericalCoordinates
@@ -12,26 +13,40 @@ from lib.relighting import ImageRelighting
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument('--file_name', type=str, required=True)
+parser.add_argument('--env_dir', type=str, required=True)
+parser.add_argument('--light_dirs', type=str, required=True)
+parser.add_argument('--ref_field', type=str, required=True)
+parser.add_argument('--ref_field_mask', type=str)
+parser.add_argument('--light_int', type=str)
+parser.add_argument('--cube_to_ll', action='store_true')
 
 if __name__ == "__main__":
 	args = parser.parse_args()
 
 	# load environment map HDR image and convert it from cube to 
 	# latitude-longitude mapping
-	im = cv2.imread('data/grace_cross.hdr', -1)[..., ::-1]
-	lum = ConvertCubeMap2LL(im, 1024)
+	lum = cv2.imread(args.env_dir, -1)[..., ::-1]
+	lum = cv2.resize(lum, (256, 128), interpolation=cv2.INTER_AREA)
+	h, w, _ = lum.shape
+	print(h,w)
+
+	if args.cube_to_ll:
+		lum = ConvertCubeMap2LL(lum, 1024)
 
 	# read reflectance field
-	reflectance_field, mask = ReadReflectanceField('data/helmet_front_left', 
-											'data/helmet_front_left_matte.png')
+	reflectance_field, mask = ReadReflectanceField(args.ref_field, 
+												mask_path=args.ref_field_mask)
+	mask = 1 - mask
 
 	# read stage light directions and intensities
-	intensities, directions = ReadLightsInfo('data/light_intensities.txt', 
-											'data/light_directions.txt')
+	intensities, directions = ReadLightsInfo(args.light_dirs, args.light_int)
 
-	
+	print("[INFO] Starting image relighting.")
+	start = time.time()
 	out = ImageRelighting(lum, directions, intensities)\
 						.relight(reflectance_field, mask)
+	end = time.time()
+	print("[INFO] Elapsed time to image relighting: ", end - start)
 
 	# save image
 	pyexr.write('out/{}.exr'.format(args.file_name), out)
